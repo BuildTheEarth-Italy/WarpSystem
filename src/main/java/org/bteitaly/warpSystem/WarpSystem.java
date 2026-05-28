7a4096f4abe569209779e8a353b4d5b93ee82eb2
Donati Filippo <65790947+DonatiFilippo@users.noreply.github.com>
2026-05-28 21:11:10 +0200

    Initial Commit

---
+++ src/main/java/org/bteitaly/warpSystem/WarpSystem.java
@@ -0,0 +1,17 @@
+package org.bteitaly.warpSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarpSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
